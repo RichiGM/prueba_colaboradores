a52f7f5a3df8ce9726c8a48db9968c1fdfc43568
RichiGM <garnicamejiaricardo@gmail.com>
2024-11-08 11:49:51 -0600

    dcfgh

---
+++ web/JS/web_JSON.js
@@ -0,0 +1,14 @@
+
+var productos = '{"productos": [' +
+    '{ "nombre": "Hamburguesa clásica", "precio": 9.99, "descripcion": "Una jugosa hamburguesa con carne de res, lechuga, tomate y queso cheddar.", "categoria": "Comida rápida" },' +
+    '{ "nombre": "Pizza Pepperoni", "precio": 12.50, "descripcion": "Pizza con salsa de tomate, queso mozzarella y rodajas de pepperoni.", "categoria": "Italiana" },' +
+    '{ "nombre": "Ensalada César", "precio": 7.95, "descripcion": "Ensalada fresca con lechuga romana, crutones, aderezo César y pollo a la parrilla.", "categoria": "Ensaladas" },' +
+    '{ "nombre": "Sushi Variado", "precio": 15.75, "descripcion": "Selección de sushi fresco incluyendo nigiri, sashimi y rollos variados.", "categoria": "Japonesa" },' +
+    '{ "nombre": "Pasta Alfredo", "precio": 11.99, "descripcion": "Pasta fettuccine en salsa Alfredo cremosa con pollo a la parrilla.", "categoria": "Italiana" },' +
+    '{ "nombre": "Filete Mignon", "precio": 22.50, "descripcion": "Filete de res premium cocido a la parrilla con guarnición de vegetales asados.", "categoria": "Carnes" },' +
+    '{ "nombre": "Tacos de Camarón", "precio": 13.25, "descripcion": "Tacos mexicanos con camarones frescos, cilantro, cebolla y salsa de aguacate.", "categoria": "Mexicana" },' +
+    '{ "nombre": "Sopa de Mariscos", "precio": 8.50, "descripcion": "Sopa caliente con una mezcla de mariscos frescos, vegetales y caldo casero.", "categoria": "Sopas" },' +
+    '{ "nombre": "Ceviche Peruano", "precio": 10.50, "descripcion": "Ceviche tradicional peruano con pescado fresco marinado en limón, cilantro y ají.", "categoria": "Peruana" },' +
+    '{ "nombre": "Pastel de Chocolate", "precio": 5.99, "descripcion": "Delicioso pastel de chocolate con cobertura de ganache y fresas frescas.", "categoria": "Postres" }' +
+']}';
+
